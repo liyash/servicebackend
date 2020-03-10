767a7e2629f615ecd6f9e52973e903a71a421573
liyashereef <lshereef@secture.ca>
2020-03-10 18:09:26 +0530

    login page and design integration

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email','email_verified_at','is_admin', 'password',
+        'username', 'email','email_verified_at','is_admin', 'password',
     ];
 
     /**
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.welcomeapp')
 
 @section('content')
-<div class="container">
+<div class="container" style="margin-top:10%">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
